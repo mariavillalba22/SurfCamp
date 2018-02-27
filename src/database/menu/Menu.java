c97d82741f9e5795c08199ebcd399353f8bc17d0
Catalina <Catalina@CATALINA>
2018-02-27 19:03:18 +0100

    menu a sucio

---
+++ src/database/menu/Menu.java
@@ -0,0 +1,23 @@
+package database.menu;
+
+import java.io.*;
+import java.util.*;
+
+public class Menu {
+
+	public static void main (String [] args) {
+	try{
+		
+		
+		System.out.println("SELECT AN OPTION: "
+				+ "1) VIEW"
+				+ "2) INSERT" // a partir de aqui hay q comprobar si hay algo
+				+ "3) MODIFY"
+				+ "4) DELETE");
+		
+		
+		
+	}catch(IOException ex)  {}
+	
+	}
+}
